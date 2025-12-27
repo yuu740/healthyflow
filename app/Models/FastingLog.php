3ecec801865f4c20c837b535000c3a61f5cf25de
Dexcop <ardellejodynathaniel@gmail.com>
2025-12-27 19:06:44 +0700

    built the needed mfs from ERD

---
+++ app/Models/FastingLog.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class FastingLog extends Model
+{
+    /** @use HasFactory<\Database\Factories\FastingLogFactory> */
+    use HasFactory;
+}
---
+++ database/factories/WaterLogFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\WaterLog>
+ */
+class WaterLogFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
